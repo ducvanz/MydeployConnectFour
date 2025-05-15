f00d88b65c3485f7327a1199279814a42eedb933
ducvanz <ducvan2745@gmail.com>
2025-05-15 07:21:04 +0700

    Update Constant.py

--- Constant.py
+++ Constant.py
@@ -3,8 +3,7 @@ from enum import Enum
 RED     = 1             # Số đại diện cho màu quân cờ trên board
 YELLOW  = - RED
 IDLE    = 0             # Ô trống, chưa đánh
-
+BLOCK = 3
 
 WIDTH = 600             # Screen width
 FIRST_MOVING = RED
-    
